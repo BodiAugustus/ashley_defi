0fc4eb4a54e9e76c4fe1f064dd663f4cca54d3f1
Bodi Augustus <bodiAugustus@protonmail.com>
2022-01-23 12:12:07 -0600

    Worked on building out the Navbar component. Added custom config to tailwind.config.js for smaller screens.

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -1,12 +1,22 @@
-
+import Image from 'next/image'
+import {FaBars} from 'react-icons/fa'
 
 const Navbar = () => {
     return(
         <nav>
-            <div className="flex justify-between p-1">
-                <div className="">Header</div>
-                <div className="">Links</div>
-                <div className="">Social Icons</div>
+            <div className="flex justify-between p-1 bg-sky-300">
+                <div className="flex items-center justify-between p-1 relative bg-red-300 w-[8%]">
+                    <Image
+                        src="/android-chrome-192x192.png"
+                        alt="Ashley Capital & DeFi Logo"                        
+                        layout='fill'
+                        />
+                </div>
+                    <button className='text-2xl cursor-pointer transition-all hover:rotate-90'>
+                        <FaBars/>
+                    </button>
+                <div className="hidden overflow-hidden transition-all ">Links</div>
+                <div className="hidden">Social Icons</div>
             </div>
         </nav>
     )
