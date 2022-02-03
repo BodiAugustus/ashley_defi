6f0bc4d9d2a1369d3f24a42de339a8a32445851b
Bodi Augustus <bodiAugustus@protonmail.com>
2022-02-03 13:30:06 -0600

    #113. Setup functonality of useManagedCOurses to fetch purchased course data from SC. Successfully returned owned courses data to console.

--- components/providers/web3/hooks/useManagedCourses.js
+++ components/providers/web3/hooks/useManagedCourses.js
@@ -9,7 +9,20 @@ export const handler = (web3, contract) => (account) => {
     const swrRes = useSWR(() => 
         (web3 && contract && account) ? `web3/managedCourses/${account}` : null,
         async () => {
-                const courses = [1,2,3,4]
+                const courses = []
+                const courseCount = await contract.methods.getCourseCount().call()
+
+                //loop returns last item first
+                for (let i = Number(courseCount) - 1; i >= 0; i--) {
+                    const courseHash = await contract.methods.getCourseHashAtIndex(i).call()
+                    const course = await contract.methods.getCourseByHash(courseHash).call()
+
+                    if(course){
+                        const normalized = normalizeOwnedCourse(web3)({hash: courseHash}, course)
+                        //returns the courses to the courses array in normalized form
+                        courses.push(normalized)
+                    }
+                }
 
             return courses
         }
