8edfe2c3db22e390649e6fd478af34d952ca3a58
Bodi Augustus <bodiAugustus@protonmail.com>
2022-02-06 15:28:39 -0600

    #172. Created body1 folder inside services. Created index.js. Created Body1 component. Finished Body1.

--- components/ui/services/body1/index.js
+++ components/ui/services/body1/index.js
@@ -1,29 +1,42 @@
 import { FaSkullCrossbones } from 'react-icons/fa'
+import { IoMdRocket } from 'react-icons/io'
 
 const Body1 = () => {
     return (
         
         <div className="h-[100vh] bg-black text-white px-3">
-            <h1 className="text-3xl text-center">We help you understand the risk, such as:</h1>
-            <div className="flex items-center mb-2">
-            <FaSkullCrossbones className='text-2xl mr-1'/>
+            <h1 className="text-3xl text-center mb-2">We help you understand the risk, such as:</h1>
+            <div className="flex items-center mb-2 font-economica">
+            <FaSkullCrossbones className='text-2xl mr-1 '/>
             <h2>Bots driving up the price at an ICO event before you even have time to make an initial offer and click trade. </h2>
             </div>
-            <div className="flex items-center mb-2">
+            <div className="flex items-center mb-2 font-economica">
             <FaSkullCrossbones className='text-xl mr-1'/>
             <h2>That anybody can make a crypto token and a website that looks good to scam money.</h2>
             </div>
-            <div className="flex items-center mb-2">
-            <FaSkullCrossbones className='text-3xl mr-1'/>
+            <div className="flex items-center mb-2 font-economica">
+            <FaSkullCrossbones className='text-4xl mr-1'/>
             <h2>How phishing sites can look identicle to your trading platforms and hack your data before stealing your funds if you&apos;re not aware of what to look for.</h2>
             </div>
-            <div className="flex items-center mb-2">
-            <FaSkullCrossbones className='text-lg mr-1'/>
-            <h2>Projects, protocols and entire chains can be hacked and some projects rug pull</h2>
+            <div className="flex items-center mb-2 font-economica">
+            <FaSkullCrossbones className='text-4xl mr-1'/>
+            <h2>That most Twitter crypto influencers get paid by developers to shill projects before liquidating as the newcomers arrive and drive the price up before the crash.</h2>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center mb-2 font-economica">
+            <FaSkullCrossbones className='text-lg mr-1'/>
+            <h2>Projects, protocols and entire chains can be hacked and some projects rug pull.</h2>
+            </div>
+            <div className="flex items-center mb-2 font-economica">
+            <FaSkullCrossbones className='text-base mr-1'/>
+            <h2>Sudden and large swings in market and token prices can happen quickly and often.</h2>
+            </div>
+            <div className="flex items-center mb-5 font-economica">
             <FaSkullCrossbones className='text-base mr-1'/>
             <h2>That there are very few regulations - you&apos;re in the Wild West now!</h2>
+            </div>
+            <div className="flex items-center font-mono text-center">
+   
+            <h2>Despite these risk and more however there really is no question, DeFi is the future and the future has begun - Come join with us!</h2>
             </div>
             
   
