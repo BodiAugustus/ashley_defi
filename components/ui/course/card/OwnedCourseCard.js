14262af36e006489fc581a69589da1fdd953356d
Bodi Augustus <bodiAugustus@protonmail.com>
2022-06-30 18:46:40 +0300

    Updated membership

--- components/ui/course/card/OwnedCourseCard.js
+++ components/ui/course/card/OwnedCourseCard.js
@@ -1,25 +1,30 @@
-import Image from "next/image"
-
+import Image from "next/image";
+import cat from "../../../../public/images/Nyan-Cat.jpeg";
 
 const STATE_COLORS = {
   purchased: "bg-indigo-500",
   activated: "bg-green-400",
-  deactivated: "bg-red-400"
-}
+  deactivated: "bg-red-400",
+};
 
-export default function OwnedCourseCard({children, course, type = "purchased"}) {
-
-  const stateColor = STATE_COLORS[course.state]
+export default function OwnedCourseCard({
+  children,
+  course,
+  type = "purchased",
+}) {
+  const stateColor = STATE_COLORS[course.state];
 
   return (
-    <div className="bg-black border border-[#2b2b2b] shadow overflow-hidden sm:rounded-lg mb-16 
-    xxs:w-[95vw] xxs:mx-auto">
+    <div
+      className="bg-black border border-[#2b2b2b] shadow overflow-hidden sm:rounded-lg mb-16 
+    xxs:w-[95vw] xxs:mx-auto"
+    >
       <div className="xs:block md:flex">
         <div className="flex-1">
           <div className="xs:h-52 md:h-full next-image-wrapper ">
             <Image
               className="object-cover"
-              src={course.coverImage}
+              src={cat}
               width="45"
               height="45"
               layout="responsive"
@@ -27,85 +32,87 @@ export default function OwnedCourseCard({children, course, type = "purchased"}) 
             />
           </div>
         </div>
-          <div className="flex-4">
-            <div className="px-1 py-5 sm:px-6">
-              <h3 className="text-lg leading-6 font-medium text-white ">
-                <span className="mr-2">{course.title}</span>
-                <span className={`text-xs ${STATE_COLORS[type]}  rounded-full xxs:p-1
+        <div className="flex-4">
+          <div className="px-1 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-white ">
+              <span className="mr-2">{course.title}</span>
+              <span
+                className={`text-xs ${STATE_COLORS[type]}  rounded-full xxs:p-1
                 xs:p-2 
-                `}>{course.state}</span>
-              </h3>
-              <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                {course.price} USD
-              </p>
-            </div>
+                `}
+              >
+                {course.state}
+              </span>
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              {course.price} USD
+            </p>
+          </div>
 
-            <div className="border-t border-[#2b2b2b]">
-              <dl>
-                <div className="bg-black px-4 py-5 ">
-                  <dt className="text-sm font-medium text-gray-500">
+          <div className="border-t border-[#2b2b2b]">
+            <dl>
+              <div className="bg-black px-4 py-5 ">
+                <dt className="text-sm font-medium text-gray-500">
                   February Fact:
-                  </dt>
-                  <dd className="mt-1 text-sm text-white sm:mt-0 sm:col-span-2">
-                  70% of the total value of Bitcoin is owned by only 2% of it&apos;s holders.
-                  </dd>
-                </div>
-                <div className="bg-black px-4 py-5 ">
-                  <dt className="text-sm font-medium text-gray-500">
-                    Purchase Proof:
-                  </dt>
-                  <dd className="xs:break-words mt-1 text-sm text-white sm:mt-0 xs:col-span-7">
+                </dt>
+                <dd className="mt-1 text-sm text-white sm:mt-0 sm:col-span-2">
+                  70% of the total value of Bitcoin is owned by only 2% of
+                  it&apos;s holders.
+                </dd>
+              </div>
+              <div className="bg-black px-4 py-5 ">
+                <dt className="text-sm font-medium text-gray-500">
+                  Purchase Proof:
+                </dt>
+                <dd className="xs:break-words mt-1 text-sm text-white sm:mt-0 xs:col-span-7">
                   {course.proof}
-                  </dd>
-                </div>
+                </dd>
+              </div>
 
-                <div className="bg-black px-4 py-5 sm:px-6">
-                  {children}
-                </div>
-              </dl>
-            </div>
+              <div className="bg-black px-4 py-5 sm:px-6">{children}</div>
+            </dl>
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
-  
-  
-  // import Image from "next/image"
-  // import Link from "next/link"
-  
-  // export default function XCard({course}) {
-  //   return (
-  //     <div
-  //       className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-  //       <div className="flex h-full">
-  //         <div className="flex h-full">
-  //           <Image
-  //             className="object-cover"
-  //             src={course.coverImage}
-  //             layout="fixed"
-  //             width="200"
-  //             height="230"
-  //             alt={course.title}
-  //           />
-  //         </div>
-  //         <div className="p-8">
-  //           <div
-  //             className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-  //             {course.type}
-  //           </div>
-  //           <Link href={`/courses/${course.slug}`}>
-  //             <a
-  //               className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
-  //               {course.title}
-  //             </a>
-  //           </Link>
-  //           <p
-  //             className="mt-2 text-gray-500">
-  //             {course.description}
-  //           </p>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   )
-  // }
+
+// import Image from "next/image"
+// import Link from "next/link"
+
+// export default function XCard({course}) {
+//   return (
+//     <div
+//       className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+//       <div className="flex h-full">
+//         <div className="flex h-full">
+//           <Image
+//             className="object-cover"
+//             src={course.coverImage}
+//             layout="fixed"
+//             width="200"
+//             height="230"
+//             alt={course.title}
+//           />
+//         </div>
+//         <div className="p-8">
+//           <div
+//             className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+//             {course.type}
+//           </div>
+//           <Link href={`/courses/${course.slug}`}>
+//             <a
+//               className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+//               {course.title}
+//             </a>
+//           </Link>
+//           <p
+//             className="mt-2 text-gray-500">
+//             {course.description}
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
--- components/ui/course/hero/index.js
+++ components/ui/course/hero/index.js
@@ -1,99 +1,130 @@
-import Image from "next/image"
+import Image from "next/image";
+import cat from "../../../../public/images/Nyan-Cat.jpeg";
 
-
-const CourseHero = ({title, description, image, hasOwner}) => {
-    return(
-        <section>
-        <div className="relative overflow-hidden">
-          <div className="max-w-7xl mx-auto">
-            <div className="relative z-10 pb-8 bg-black 
+const CourseHero = ({ title, description, image, hasOwner }) => {
+  return (
+    <section>
+      <div className="relative overflow-hidden">
+        <div className="max-w-7xl mx-auto">
+          <div
+            className="relative z-10 pb-8 bg-black 
             sm:pb-16 
             md:pb-20 
             lg:max-w-2xl lg:w-full lg:pb-28 
-            xl:pb-32">
-              <svg className="hidden absolute right-0 inset-y-0 h-full w-48 text-indigo-500 transform translate-x-1/2
+            xl:pb-32"
+          >
+            <svg
+              className="hidden absolute right-0 inset-y-0 h-full w-48 text-indigo-500 transform translate-x-1/2
               
-              lg:block" 
-              fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-                <polygon 
-                className="bg-black"
-                points="50,0 100,0 50,100 0,100" />
-              </svg>
-              <div className="relative pt-6 px-4 
+              lg:block"
+              fill="currentColor"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              aria-hidden="true"
+            >
+              <polygon className="bg-black" points="50,0 100,0 50,100 0,100" />
+            </svg>
+            <div
+              className="relative pt-6 px-4 
               sm:px-6 
-              lg:px-8">
-              </div>
-              <main className="mt-10 mx-auto max-w-7xl px-4 bg-black 
+              lg:px-8"
+            ></div>
+            <main
+              className="mt-10 mx-auto max-w-7xl px-4 bg-black 
               sm:mt-12 sm:px-6 
               md:mt-16 
               lg:mt-0 lg:px-8 
-              xl:mt-0">
-                <div className="sm:text-center lg:text-left">
-                <div className="text-lg inline-block p-4 py-2 rounded-md font-bold bg-red-300 text-red-700 float-left
+              xl:mt-0"
+            >
+              <div className="sm:text-center lg:text-left">
+                <div
+                  className="text-lg inline-block p-4 py-2 rounded-md font-bold bg-red-300 text-red-700 float-left
                 xxs:-mt-10 xxs:mb-10
-                lg:hidden ">
+                lg:hidden "
+                >
                   Current Forecast: Wait / Sell
                 </div>
-                {hasOwner &&
-                <div className="text-lg inline-block p-4 py-2 rounded-full font-bold bg-green-300 text-green-700
-                sm:mb-4">
-                  Current Member of:
-                </div>
-                }
-                  <h1 className="text-4xl tracking-tight font-extrabold text-white
+                {hasOwner && (
+                  <div
+                    className="text-lg inline-block p-4 py-2 rounded-full font-bold bg-green-300 text-green-700
+                sm:mb-4"
+                  >
+                    Current Member of:
+                  </div>
+                )}
+                <h1
+                  className="text-4xl tracking-tight font-extrabold text-white
                   xs:text-5xl 
-                  md:text-6xl">
-                    <span className="block xl:inline">
-                    {title.substring(0, title.length/2)}
-                    </span>
-                    <span className="block text-indigo-600 xl:inline">
+                  md:text-6xl"
+                >
+                  <span className="block xl:inline">
+                    {title.substring(0, title.length / 2)}
+                  </span>
+                  <span className="block text-indigo-600 xl:inline">
                     {title.substring(title.length / 2)}
-                    </span>
-                  </h1>
-                  <p className="mt-3 text-base text-white 
+                  </span>
+                </h1>
+                <p
+                  className="mt-3 text-base text-white 
                   
                   sm:mt-5 sm:text-xl sm:max-w-xl sm:mx-auto 
                   md:mt-5 md:text-2xl 
-                  lg:mx-0 lg:text-xl">
-                  Welcome and hello friends, members, and fellow DeFi and crypto enthusiast! This month looks to be another exciting and eventful time in the space as Bitcoin and Ethereum continue to move volumes and alt-coins rebound strong off their recent devaluations.
-                  </p>
-                  <p className="mt-3 text-base text-white 
+                  lg:mx-0 lg:text-xl"
+                >
+                  Welcome and hello friends, members, and fellow DeFi and crypto
+                  enthusiast! This month looks to be another exciting and
+                  eventful time in the space as Bitcoin and Ethereum continue to
+                  move volumes and alt-coins rebound strong off their recent
+                  devaluations.
+                </p>
+                <p
+                  className="mt-3 text-base text-white 
                   sm:mt-5 sm:text-xl sm:max-w-xl sm:mx-auto md:mt-5 md:text-2xl 
-                  lg:mx-0">
-                  <strong>It look&apos;s like it&apos;s going to be a busy month!</strong>
-                  </p>
-                  <div className="mt-5 sm:mt-8 
+                  lg:mx-0"
+                >
+                  <strong>
+                    It look&apos;s like it&apos;s going to be a busy month!
+                  </strong>
+                </p>
+                <div
+                  className="mt-5 sm:mt-8 
                   xs:w-[70%] xs:mx-auto
                   sm:flex sm:justify-center 
-                  lg:justify-start">
-                    <div className="rounded-md shadow">
-                      <a href="https://t.me/+89dQjvU76cwxZGU9" target="_blank"
-                      rel="noreferrer" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 
-                      md:py-4 md:text-lg md:px-10">
-                        Speak to a Consultant
-                      </a>
-                    </div>
-
+                  lg:justify-start"
+                >
+                  <div className="rounded-md shadow">
+                    <a
+                      href="https://t.me/+89dQjvU76cwxZGU9"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 
+                      md:py-4 md:text-lg md:px-10"
+                    >
+                      Speak to a Consultant
+                    </a>
                   </div>
                 </div>
-              </main>
-            </div>
+              </div>
+            </main>
           </div>
-          <div className="
-          lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-            <Image 
+        </div>
+        <div
+          className="
+          lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
+        >
+          <Image
             className="h-56 w-full object-cover 
             sm:h-72 
             md:h-96 
             lg:w-full lg:h-full"
-            layout="fill" 
-            src={image} 
-            alt={title} />
-          </div>
+            layout="fill"
+            src={cat}
+            alt={title}
+          />
         </div>
-      </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
-
-export default CourseHero
+export default CourseHero;
